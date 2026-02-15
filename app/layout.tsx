6b321b4d8496fcb30a8ae3c06267fd71c3c6fb92
Sameer Mohammad <mohammadsameer0409@gmail.com>
2026-02-15 16:26:06 -0600

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 import { ThemeProvider } from "@/context/ThemeContext";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Notz",
+  description: "Simple cloud notes app built by Sameer",
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
